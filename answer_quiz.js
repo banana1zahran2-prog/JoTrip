392c30f18521badd632ce1ffa9be16d7d4ac4824
Banan Iyad <banana1zahran2@gmail.com>
2026-08-04 11:30:07 +0300

    Add files via upload

--- answer_quiz.js
+++ answer_quiz.js
@@ -14,58 +14,6 @@ btn_submit.addEventListener("click",function(){
 });
 /*********************************************************************************************/
 
-/* اختيارات اليوزر داخل متغيرات */  /*
-document.getElementById("submit").addEventListener("click",function(){
-    let Budget = document.querySelector(`input[name=budget]:checked`).value;
-    let Weather = document.querySelector(`input[name="weather"]:checked`).value;
-    let Activity = document.querySelector(`input[name="activity"]:checked`).value;
-    let Type = document.querySelector(`input[name="type"]:checked`).value;
-    let Camping = document.querySelector('input[name="camping"]:checked').value;*/
-
-/* نقارن الاختيارات مع الاماكن 
-let results = [];
-places.forEach(place=>{   /*place : المكان الحالي الذي يتم فحصه */ /*
-let score =0;
-
-     if(place.budget == Budget){ score++; }
-     if(place.weather == Weather){ score++; }
-     if(place.activity.includes(Activity)){ score++;}
-     if(place.type.includes(Type)){ score++; }
-     if(place.camping == Camping){ score++; }
-
-     if(score>=3){ results.push(place); }
-});
-*/
-
-/*عرض النتيجة */
-/*
-let output = "";
-if(results.length==0){output=`<h5> No destinations match your preferences , try again</h5>`;}
-else{
-results.slice(0,6).forEach(place => {
-    output += `
-    <div class="place-card">
-        <h2>${place.name}</h2> <br>
-        <p>${place.description}</p> <br>
-        <div class="img-result">
-           <div class ="img"> 
-                <img src="${place.image[0]}">
-                <img src="${place.image[1]}">
-                <img src="${place.image[2]}">
-                    <!-- تكرار الصور -->
-                    <img src="${place.image[0]}">
-                    <img src="${place.image[1]}">
-                    <img src="${place.image[2]}">
-            </div>
-         </div>
-    </div>
-    `;
-});
-}
-document.getElementById("result").innerHTML = output;*/
-
-
-
 
 
 document.getElementById("submit").addEventListener("click", function () {
@@ -97,7 +45,7 @@ document.getElementById("submit").addEventListener("click", function () {
             if(place.camping == Camping){ score++; }
             if(score >= 3){ results.push(place); }
         });
-        
+
         if(results.length == 0){ output = `<h5>No destinations match your preferences , try again</h5>`; }
         /*عرض النتيجة */
         else {
